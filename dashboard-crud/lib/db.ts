06dc130cc24c77ed533dd11ce2b7cf0309320a69
hashaaan <shalithadev@gmail.com>
2025-10-03 14:49:51 +0530

    Setup Drizzle

---
+++ dashboard-crud/lib/db.ts
@@ -0,0 +1,15 @@
+// Make sure to install the 'pg' package
+import { drizzle } from "drizzle-orm/node-postgres";
+// You can specify any property from the node-postgres connection options
+const db = drizzle({
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
+});
+
+const result = await db.execute("select 1");
+console.log(result);
+
+export { db };
+export type { InferModel } from "drizzle-orm";
