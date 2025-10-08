cf4156079280c924706c6f1aead1ec6c3a4fdf66
hashaaan <shalithadev@gmail.com>
2025-10-08 10:39:22 +0530

    Add use effect once

---
+++ dashboard-crud/hooks/use-effect-once/index.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect, useRef } from "react";
+
+export default function EffectOnce() {
+  const [input, setInput] = useState("");
+  const didMountRef = useRef(false);
+
+  useEffect(() => {
+    // Check if the component has mounted before running the effect.
+    if (didMountRef.current) {
+      console.log("Input was updated to:", input);
+    } else {
+      didMountRef.current = true;
+    }
+  }, [input]); // The effect is triggered by 'input' changes.
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </div>
+  );
+}
